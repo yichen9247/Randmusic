c35919bd980f43490649751a3ab36de4e38a96ff
Mr.Hua <114044950+yichen9247@users.noreply.github.com>
2023-03-10 18:15:18 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -37,16 +37,23 @@
     
     <meta property="og:site_name" content="随机网易云"/>
 	<title>随机网易云</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/layui/2.7.6/css/layui.css">
-    <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/mdui@1.0.0/dist/css/mdui.min.css">
-    <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <?php
-      if($website_icons == true) {
-        echo '    <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.1.2/css/all.min.css">';
-      }
-      echo "\n";
-    ?>
     <link rel="shortcut icon" href="https://s1.music.126.net/style/favicon.ico?v20180823">
+    <?php
+      if($m == 1) {
+        echo "\n";
+      } else 
+      if($m == 0) {
+        echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/layui/2.7.6/css/layui.css">';
+        echo "\n";
+        echo '    <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/mdui@1.0.0/dist/css/mdui.min.css">';
+        echo "\n";
+        echo '    <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">';
+        echo "\n";
+        if($website_icons == true) {
+          echo '    <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.1.2/css/all.min.css">';
+        }
+      }
+    ?>
   </head>
   <?php
     if($website_recolor == true) {
@@ -149,76 +156,76 @@
     } else
     if($m == 0) {
       $live2d_vOffset = -50;
-    }
-    if($website_online == true) {
-      if($website_icons == true) {
-        echo '    <button id="copyright-relode" class="btn btn-outline-info btn-lg btn-block mdui-ripple" onclick="javascript:window.location.reload();"><i class="mdui-icon material-icons fa-spin">face</i>&nbsp;&nbsp;随机网易云（'.online_users().'人在线）</button>';
-      } else {
+      if($website_online == true) {
+        if($website_icons == true) {
+          echo '    <button id="copyright-relode" class="btn btn-outline-info btn-lg btn-block mdui-ripple" onclick="javascript:window.location.reload();"><i class="mdui-icon material-icons fa-spin">face</i>&nbsp;&nbsp;随机网易云（'.online_users().'人在线）</button>';
+        } else {
         echo '    <button id="copyright-relode" class="btn btn-outline-info btn-lg btn-block mdui-ripple" onclick="javascript:window.location.reload();">随机网易云（'.online_users().'人在线）</button>';
+        }
+        echo "\n";
+        echo '    </div>';
+        echo "\n  ";
+        } else {
+        if($website_icons == true) {
+          echo '    <button id="copyright-relode" class="btn btn-outline-info btn-lg btn-block mdui-ripple" onclick="javascript:window.location.reload();"><i class="mdui-icon material-icons fa-spin">face</i>&nbsp;&nbsp;随机网易云</button>';
+        } else {
+          echo '    <button id="copyright-relode" class="btn btn-outline-info btn-lg btn-block mdui-ripple" onclick="javascript:window.location.reload();">随机网易云</button>';
+        }
+        echo "\n";
+        echo '    </div>';
+        echo "\n  ";
+        }
+      if($website_debug == 1) {
+        echo '  <div class="debug"><hr/>';
+        echo "\n    ";
+        echo '  <h5 class="mdui-text-color-white" style="text-align:center;">';
+        echo "\n  ";
+        echo '      ['.$song.']ID:'.$a[$music].'';
+        echo '      </h5>';
+        echo "\n    ";
+        echo '</div>';
+        echo "\n";
+        }
+      if($live2d_status == true) {
+        echo '  <script src="https://assets.1ilo.com/live2d/wp-3d-pony/live2dw/lib/L2Dwidget.min.js"></script>';
+        echo "\n";
       }
-      echo "\n";
-      echo '    </div>';
-      echo "\n  ";
-      } else {
-      if($website_icons == true) {
-        echo '    <button id="copyright-relode" class="btn btn-outline-info btn-lg btn-block mdui-ripple" onclick="javascript:window.location.reload();"><i class="mdui-icon material-icons fa-spin">face</i>&nbsp;&nbsp;随机网易云</button>';
-      } else {
-        echo '    <button id="copyright-relode" class="btn btn-outline-info btn-lg btn-block mdui-ripple" onclick="javascript:window.location.reload();">随机网易云</button>';
+      if($texiao_dianji == 1) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_dianji.js"></script>';
+        echo "\n";
+      } else
+      if($texiao_dianji == 2) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_aixin.js"></script>';
+        echo "\n";
+      } else
+      if($texiao_dianji == 3) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_yinghua.js"></script>';
+        echo "\n";
+      } else
+      if($texiao_dianji == 4) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_zhizhu.js"></script>';
+        echo "\n";
+      } else
+      if($texiao_dianji == 5) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_xiannu.js"></script>';
+        echo "\n";
+      } else
+      if($texiao_dianji == 6) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_face.js"></script>';
+        echo "\n";
+      } else
+      if($texiao_dianji == 7) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_paopao.js"></script>';
+        echo "\n";
+      } else
+      if($texiao_dianji == 8) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_yanhua.js"></script>';
+        echo "\n";
+      } else
+      if($texiao_dianji == 9) {
+        echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_text.js"></script>';
+        echo "\n";
       }
-      echo "\n";
-      echo '    </div>';
-      echo "\n  ";
-      }
-    if($website_debug == 1) {
-      echo '  <div class="debug"><hr/>';
-      echo "\n    ";
-      echo '  <h5 class="mdui-text-color-white" style="text-align:center;">';
-      echo "\n  ";
-      echo '      ['.$song.']ID:'.$a[$music].'';
-      echo '      </h5>';
-      echo "\n    ";
-      echo '</div>';
-      echo "\n";
-      }
-    if($live2d_status == true) {
-      echo '  <script src="https://assets.1ilo.com/live2d/wp-3d-pony/live2dw/lib/L2Dwidget.min.js"></script>';
-      echo "\n";
-    }
-    if($texiao_dianji == 1) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_dianji.js"></script>';
-      echo "\n";
-    } else
-    if($texiao_dianji == 2) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_aixin.js"></script>';
-      echo "\n";
-    } else
-    if($texiao_dianji == 3) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_yinghua.js"></script>';
-      echo "\n";
-    } else
-    if($texiao_dianji == 4) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_zhizhu.js"></script>';
-      echo "\n";
-    } else
-    if($texiao_dianji == 5) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_xiannu.js"></script>';
-      echo "\n";
-    } else
-    if($texiao_dianji == 6) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_face.js"></script>';
-      echo "\n";
-    } else
-    if($texiao_dianji == 7) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_paopao.js"></script>';
-      echo "\n";
-    } else
-    if($texiao_dianji == 8) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_yanhua.js"></script>';
-      echo "\n";
-    } else
-    if($texiao_dianji == 9) {
-      echo '  <script src="https://fastly.jsdelivr.net/gh/yichen9247/Randmusic/assets/JavaScript/texiao_text.js"></script>';
-      echo "\n";
     }
   ?>
   <script type="text/javascript">
@@ -263,7 +270,16 @@
       }
     ?>
   </script>
-  <script src="https://cdn.staticfile.org/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://fastly.jsdelivr.net/npm/mdui@1.0.0/dist/js/mdui.min.js"></script>
+  <?php
+    if($m == 1) {
+      echo "\n";
+    } else 
+    if($m == 0) {
+      echo '<script src="https://cdn.staticfile.org/jquery/3.6.0/jquery.min.js"></script>';
+      echo "\n";
+      echo '  <script src="https://fastly.jsdelivr.net/npm/mdui@1.0.0/dist/js/mdui.min.js"></script>';
+      echo "\n";
+    }
+  ?>
   <!-- 2019-2022 © Reah-随机网易云(https://music.yunair.cn) -->
   </body><?php echo '<!-- 加载耗时：'.timer_get().'-->';?></html>
