93f113757dbb46017fbf5a85cf3177ea990860a1
Mr.Hua <114044950+yichen9247@users.noreply.github.com>
2023-04-08 17:20:15 +0800

    Add files via upload

--- Core/Config/config.php
+++ Core/Config/config.php
@@ -1,12 +1,18 @@
 <?php
-$website_url = "http://"; # 请在这里填写网站链接
-$texiao_dianji = 0;  # 1:气泡点击特效 2:爱心点击特效 3:樱花点击特效 4:蜘网点击特效 5:仙女点击特效 6:笑脸点击特效 7:泡泡点击特效 8:烟花点击特效 9:文字点击特效
+
+# ==================== 以下为网站基础设置 ====================
+
+$website_url = "http(s)://"; # 请在这里填写网站链接
+$website_click = 0;  # 1:气泡点击特效 2:爱心点击特效 3:樱花点击特效 4:蜘网点击特效 5:仙女点击特效 6:笑脸点击特效 7:泡泡点击特效 8:烟花点击特效 9:文字点击特效
 $website_icons = false; # 按钮内旋转图标显示开关
 $website_mdui2 = false; # 启用MDUI2样式（美化）
 $website_feedback = false; # 网站音乐反馈按钮开关
 $website_online = false; # 网站在线人数显示开关
-$live2d_status = false; # 看板娘Live2D显示开关
 $website_recolor = false; # 网站全局黑白模式开关
+
+# ==================== 以下为网站小娘设置 ====================
+
+$live2d_status = false; # 看板娘Live2D显示开关
 $live2d_width = 100; # 看板娘宽度，默认是：100
 $live2d_height = 200; # 看板娘高度，默认是：200
 $live2d_default = 0.7; # 默认透明度：0.1-1.0
@@ -14,16 +20,24 @@ $live2d_onhover = 0.2; # 显示透明度：0.1-1.0
 $live2d_position = "left"; # 看板娘方向 左下：left 右下：right
 $live2d_modelurl = "https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json"; # 看板娘Live2D模板链接，详情请查看下方模板列表，当然你也可以用第三方链接
 
+# ==================== 以下为网站安全设置 ====================
+
+$cc_project = false; # 是否开启网站CC防护功能
+$website_dissql = false; #是否开启SQL防注入功能
+
 # ==================== 以下为发信邮箱设置 ====================
 
 $mail_host = "smtp.163.com"; # 邮件主机名，例如：smtp.163.com
 $mail_port = 25; # 邮件发送端口，详情自行百度
 $mail_secure = "TLS"; # 邮件发送协议：TLS/SSL
 $mail_isHTML = true; # 设置邮件发送为HTML模式
-$mail_adminmai = ""; # 管理员邮箱账号
-$mail_username = ""; # 邮箱账号/地址
-$mail_password = ""; # 邮箱密码/授权码
-$mail_fromuser = ""; # 邮箱地址/账号
+$mail_adminmai = "1234567890@qq.com"; # 管理员邮箱账号
+$mail_username = "yunairsite@163.com"; # 邮箱账号/地址
+$mail_password = "password"; # 邮箱密码/授权码
+$mail_fromuser = "yunairsite@163.com"; # 邮箱地址/账号
+
+# ==================== 以下为三方模板链接 ====================
+
 # 黑猫：https://unpkg.com/live2d-widget-model-hijiki@1.0.5/assets/hijiki.model.json
 # 萌娘：https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json
 # 白猫：https://unpkg.com/live2d-widget-model-tororo@1.0.5/assets/tororo.model.json
