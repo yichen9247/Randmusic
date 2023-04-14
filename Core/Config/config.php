995481e1aca2958873ab134dcbde10a51e37033a
Mr.Hua <114044950+yichen9247@users.noreply.github.com>
2023-04-14 21:54:16 +0800

    Add files via upload

--- Core/Config/config.php
+++ Core/Config/config.php
@@ -1,40 +1,87 @@
 <?php
+require_once __CORE_DIR__.'/Config/apikey.php';
+try {
+    
+    # ==================== 以下为网站基础设置 ====================
+    $website_config = array(
+        'website_url' => 'https://music.yunair.cn', # 请在这里填写网站链接
+        'website_click' => 8, # 1:气泡点击特效 2:爱心点击特效 3:樱花点击特效 4:蜘网点击特效 5:仙女点击特效 6:笑脸点击特效 7:泡泡点击特效 8:烟花点击特效 9:文字点击特效
+        'website_icons' => false, # 按钮内旋转图标显示开关
+        'website_mdui2' => true, # 启用MDUI2样式（美化）
+        'website_feedback' => true, # 网站音乐反馈按钮开关
+        'website_online' => true, # 网站在线人数显示开关
+        'website_recolor' => false # 网站全局黑白模式开关
+    );
+    // checkConfig($website_config);
 
-# ==================== 以下为网站基础设置 ====================
+    # ==================== 以下为网站小娘设置 ====================
+    
+    $website_live2d = array(
+        'live2d_status' => true, # 看板娘Live2D显示开关
+        'live2d_width' => 100, # 看板娘宽度，默认是：100
+        'live2d_height' => 200, # 看板娘高度，默认是：200
+        'live2d_default' => 0.7, # 默认透明度：0.1-1.0
+        'live2d_onhover' => 0.2, # 显示透明度：0.1-1.0
+        'live2d_position' => 'left', # 看板娘方向 左下：left 右下：right
+        'live2d_modelurl' => 'https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json' # 看板娘Live2D模板链接，详情请查看下方模板列表，当然你也可以用第三方链接
+    );
+    // checkConfig($website_live2d);
+    
+    # ==================== 以下为网站加速设置 ====================
+    
+    $website_speed = array(
+        'website_cdn' => 4, # 0：默认，本服务器加载资源 1：Jsdelivr全球加速源 2.Jsdelivr国内加速源 3：Jsdelivr的CF加速源 4：自定义CDN源
+        'website_cdnurl' => 'https://yunairsite-1309511642.cos.ap-chengdu.myqcloud.com/163Music'
+    );
+    
+    # ==================== 以下为网站安全设置 ====================
+    
+    $website_webwaf = array(
+        'cc_project' => true, # 是否开启网站CC防护功能
+        'website_dissql' => true # 是否开启SQL防注入功能
+    );
+    // checkConfig($website_webwaf);
+    
+    # ==================== 以下为发信邮箱设置 ====================
+    
+    $website_mail = array(
+        'mail_host' => 'smtp.163.com', # 邮件主机名，例如：smtp.163.com
+        'mail_port' => 25, # 邮件发送端口，详情请自行百度
+        'mail_secure' => 'TLS', # 邮件发送协议：TLS/SSL
+        'mail_isHTML' => true, # 设置邮件发送为HTML模式
+        'mail_adminmai' => '865252486@qq.com', # 管理员邮箱账号
+        'mail_username' => 'yunairsite@163.com', # 邮箱账号/地址
+        'mail_password' => 'XVLIMOIUHROBCVFB', # 邮箱密码/授权码
+        'mail_fromuser' => 'yunairsite@163.com' # 邮箱地址/账号
+    );
+    // checkConfig($website_mail);
+    
+} catch (Exception $e) {
+    // require_once __CORE_DIR__.'/Config/throwerr.php';
+    // throwError($e->getMessage());
+}
 
-$website_url = "http(s)://"; # 请在这里填写网站链接
-$website_click = 0;  # 1:气泡点击特效 2:爱心点击特效 3:樱花点击特效 4:蜘网点击特效 5:仙女点击特效 6:笑脸点击特效 7:泡泡点击特效 8:烟花点击特效 9:文字点击特效
-$website_icons = false; # 按钮内旋转图标显示开关
-$website_mdui2 = false; # 启用MDUI2样式（美化）
-$website_feedback = false; # 网站音乐反馈按钮开关
-$website_online = false; # 网站在线人数显示开关
-$website_recolor = false; # 网站全局黑白模式开关
+switch ($website_speed['website_cdn']) {
+    case 0:
+        $cdn_url = $website_config['website_url'];
+        break;
+    case 1:
+        $cdn_url = 'https://cdn.jsdelivr.net/gh/yichen9247/Randmusic';
+        break;
+    case 2:
+        $cdn_url = 'https://fastly.jsdelivr.net/gh/yichen9247/Randmusic';
+        break;
+    case 3:
+        $cdn_url = 'https://cloudflare.jsdelivr.net/gh/yichen9247/Randmusic';
+        break;
+    case 4:
+        $cdn_url = $website_speed['website_cdnurl'];
+        break;
+    default:
+        $cdn_url = $website_config['website_url'];
+         break;
+    }
 
-# ==================== 以下为网站小娘设置 ====================
-
-$live2d_status = false; # 看板娘Live2D显示开关
-$live2d_width = 100; # 看板娘宽度，默认是：100
-$live2d_height = 200; # 看板娘高度，默认是：200
-$live2d_default = 0.7; # 默认透明度：0.1-1.0
-$live2d_onhover = 0.2; # 显示透明度：0.1-1.0
-$live2d_position = "left"; # 看板娘方向 左下：left 右下：right
-$live2d_modelurl = "https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json"; # 看板娘Live2D模板链接，详情请查看下方模板列表，当然你也可以用第三方链接
-
-# ==================== 以下为网站安全设置 ====================
-
-$cc_project = false; # 是否开启网站CC防护功能
-$website_dissql = false; #是否开启SQL防注入功能
-
-# ==================== 以下为发信邮箱设置 ====================
-
-$mail_host = "smtp.163.com"; # 邮件主机名，例如：smtp.163.com
-$mail_port = 25; # 邮件发送端口，详情自行百度
-$mail_secure = "TLS"; # 邮件发送协议：TLS/SSL
-$mail_isHTML = true; # 设置邮件发送为HTML模式
-$mail_adminmai = "1234567890@qq.com"; # 管理员邮箱账号
-$mail_username = "yunairsite@163.com"; # 邮箱账号/地址
-$mail_password = "password"; # 邮箱密码/授权码
-$mail_fromuser = "yunairsite@163.com"; # 邮箱地址/账号
 
 # ==================== 以下为三方模板链接 ====================
 
---
+++ Core/Config/functions.php
@@ -0,0 +1,32 @@
+<?php
+function checkConfig($config) {
+    $url_rule = '/^(http|https):\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\’:+!]*([^<>\”])*$/';
+    if(preg_match($url_rule,$config['website_url'])) {
+    } else {
+        throw new Exception ("网站URL地址配置错误");
+    }
+}
+
+function cdnurlChoice() {
+    switch ($website_speed['website_cdn']) {
+        case 0:
+            $cdn_url = $website_config['website_url'];
+            break;
+        case 1:
+            $cdn_url = 'https://cdn.jsdelivr.net/gh/yichen9247/Randmusic';
+            break;
+        case 2:
+            $cdn_url = 'https://fastly.jsdelivr.net/gh/yichen9247/Randmusic';
+            break;
+        case 3:
+            $cdn_url = 'https://cloudflare.jsdelivr.net/gh/yichen9247/Randmusic';
+            break;
+        case 4:
+            $cdn_url = $website_speed['website_cdnurl'];
+            break;
+        default:
+            $cdn_url = $website_config['website_url'];
+            break;
+    }
+}
+?>
