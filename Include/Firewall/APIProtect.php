e0258c3382889dbd4eff81085f759eab64768869
Mr.Hua <114044950+yichen9247@users.noreply.github.com>
2023-04-14 22:45:09 +0800

    Add files via upload

--- Include/Firewall/APIProtect.php
+++ Include/Firewall/APIProtect.php
@@ -2,40 +2,40 @@
 // 开始SESSION会话
 session_start();
 // 获取当前的时间戳
-$timestamp = time();
+$timestamps = time();
 
 // debug
-// $_SESSION['cc_locktime'] = 0;
+// $_SESSION['api_locktime'] = 0;
 
-$cc_nowtime = $timestamp;
+$api_nowtime = $timestamps;
 if (isset($_SESSION['api_lasttime'])) {
-	$cc_lasttime = $_SESSION['api_lasttime'];
-	$cc_times = $_SESSION['api_times'] + 1;
-	$_SESSION['api_times'] = $cc_times;
+	$api_lasttime = $_SESSION['api_lasttime'];
+	$api_times = $_SESSION['api_times'] + 1;
+	$_SESSION['api_times'] = $api_times;
 } else {
-	$cc_lasttime = $cc_nowtime;
-	$cc_times = 1;
-	$_SESSION['api_times'] = $cc_times;
-	$_SESSION['api_lasttime'] = $cc_lasttime;
+	$api_lasttime = $api_nowtime;
+	$api_times = 1;
+	$_SESSION['api_times'] = $api_times;
+	$_SESSION['api_lasttime'] = $api_lasttime;
 }
 
 // 判断是否处于封禁时间
-if ($_SESSION['api_locktime'] >= $timestamp) {
+if ($_SESSION['api_locktime'] >= $timestamps) {
 	header('HTTP/1.0 444');
 	exit;
 }
 
 // 判断时间间隔
-if (($cc_nowtime - $cc_lasttime) < 30) {
+if (($api_nowtime - $api_lasttime) < 30) {
 	// 判断访问次数
-	if ($cc_times >= 5) {
+	if ($api_times >= 5) {
 		// 达到访问限制，进行封锁，‘+‘后面为封禁时间，单位是秒
-		$_SESSION['api_locktime'] = $timestamp + 60;
+		$_SESSION['api_locktime'] = $timestamps + 60;
 		header('HTTP/1.0 444');
 		exit;
 	}
 } else {
-	$cc_times = 0;
-	$_SESSION['api_lasttime'] = $cc_nowtime;
-	$_SESSION['api_times'] = $cc_times;
+	$api_times = 0;
+	$_SESSION['api_lasttime'] = $api_nowtime;
+	$_SESSION['api_times'] = $api_times;
 }
