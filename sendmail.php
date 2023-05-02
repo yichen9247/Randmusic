5ea4f66654f884c20f9b363fd095ca0499624004
Hua <114044950+yichen9247@users.noreply.github.com>
2023-05-03 00:28:00 +0800

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -32,6 +32,12 @@ $mail->SMTPSecure = $website_mail['mail_secure'];
 $mail->Username = $website_mail['mail_username'];
 $mail->Password = $website_mail['mail_password'];
 
+if ($_COOKIE['usertoken'] != md5($mid)) {
+    die(json_encode(array(
+		'code' => 502,
+		'msg' => 'ERROR非法请求'
+	),320 | JSON_PRETTY_PRINT));
+} else
 if ($_GET['type'] == "" || $_GET['mid'] == "") {
     //echo 'ERROR非法请求！';
     die(json_encode(array(
@@ -42,19 +48,18 @@ if ($_GET['type'] == "" || $_GET['mid'] == "") {
 if ($_GET['type'] == 1) {
     $song = $_GET['song'];
     $mail->Subject = '【随机网易云】反馈通知 IP：'.$GetUserIP;
-    $mail->Body = '某热心网友反馈了音乐，该音乐可能有问题<br/><center><hr/>失效的ID ：['.$song.']'.$mid.'<hr/></center><br/>PS：请仔细核实该ID，若该ID确实已经失效，还请站长删除该ID以提升体验！<br/><br/>温馨提示：请将此邮箱加入到邮箱白名单，以免被当作成广告邮件！';
+    $mail->Body = '<style>.mail{width:550px;margin:0 auto;border-radius:8px;overflow:hidden;font-family:"Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei",Arial,sans-serif;box-shadow:0 2px 12px 0 rgba(0,0,0,0.1);word-break:break-all}.mail_title{color:#fff;background:linear-gradient(-45deg,rgba(9,69,138,0.2),rgba(68,155,255,0.7),rgba(117,113,251,0.7),rgba(68,155,255,0.7),rgba(9,69,138,0.2));background-size:400% 400%;background-position:50% 100%;padding:15px;font-size:15px;line-height:1.5}</style><br/><br/><div class="mail"><div class="mail_title">【随机网易云】反馈通知 IP：'.$GetUserIP.'</div><div style="background: #fff;padding: 20px;font-size: 13px;color: #666;"><div style="margin-bottom: 20px;line-height: 1.5;">某热心网友在您的站点上反馈了音乐，该音乐可能存在有失效问题：</div><div style="padding: 15px;margin-bottom: 20px;line-height: 1.5;background: repeating-linear-gradient(145deg, #f2f6fc, #f2f6fc 15px, #fff 0, #fff 25px);">ID：['.$song.']'.$mid.'&nbsp;&nbsp;<span style="color: #12addb;">0：全部音乐 1：纯音类型 2：英文歌曲</span></div><div style="line-height: 2">PS：请将此邮箱加入到邮箱白名单，以免被当作成广告邮件！<br>温馨提示：请仔细核实该ID，若该ID确实已经失效，还请站长删除该ID以提升体验！</div></div></div>';
 } else 
 if ($_GET['type'] == 2) {
     $mail->Subject = '【随机网易云】反馈通知 IP：'.$GetUserIP;
-    $mail->Body = '某热心网友反馈了歌单，该歌单可能有问题<br/><center><hr/>失效的ID ：'.$mid.'<hr/></center><br/>PS：请仔细核实该ID，若该ID确实已经失效，还请站长删除该ID以提升体验！<br/><br/>温馨提示：请将此邮箱加入到邮箱白名单，以免被当作成广告邮件！';
+    $mail->Body = '<style>.mail{width:550px;margin:0 auto;border-radius:8px;overflow:hidden;font-family:"Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei",Arial,sans-serif;box-shadow:0 2px 12px 0 rgba(0,0,0,0.1);word-break:break-all}.mail_title{color:#fff;background:linear-gradient(-45deg,rgba(9,69,138,0.2),rgba(68,155,255,0.7),rgba(117,113,251,0.7),rgba(68,155,255,0.7),rgba(9,69,138,0.2));background-size:400% 400%;background-position:50% 100%;padding:15px;font-size:15px;line-height:1.5}</style><br/><br/><div class="mail"><div class="mail_title">【随机网易云】反馈通知 IP：'.$GetUserIP.'</div><div style="background: #fff;padding: 20px;font-size: 13px;color: #666;"><div style="margin-bottom: 20px;line-height: 1.5;">某热心网友在您的站点上反馈了歌单，该歌单可能存在有失效问题：</div><div style="padding: 15px;margin-bottom: 20px;line-height: 1.5;background: repeating-linear-gradient(145deg, #f2f6fc, #f2f6fc 15px, #fff 0, #fff 25px);">ID：'.$mid.'&nbsp;&nbsp;<span style="color: #12addb;">0：全部歌单 1：纯音歌单 2：英文歌单</span></div><div style="line-height: 2">PS：请将此邮箱加入到邮箱白名单，以免被当作成广告邮件！<br>温馨提示：请仔细核实该ID，若该ID确实已经失效，还请站长删除该ID以提升体验！</div></div></div>';
 } else {
     //echo 'ERROR非法请求！';
     die(json_encode(array(
-		'code' => 500,
+		'code' => 501,
 		'msg' => 'ERROR非法请求'
 	),320 | JSON_PRETTY_PRINT));
 }
 
 $status = $mail->send();
-
 ?>
